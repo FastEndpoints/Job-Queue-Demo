6533b2aa267d0350a128885269037bbd19745fac
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2024-06-25 12:38:31 +0530

    minor refactor

--- src/Commands/LongRunningCommand.cs
+++ src/Commands/LongRunningCommand.cs
@@ -1,6 +1,6 @@
 ﻿namespace JobQueueDemo;
 
-public sealed class LongRunningCommand : ICommand;
+sealed class LongRunningCommand : ICommand;
 
 sealed class LongRunningCommandHandler(ILogger<LongRunningCommandHandler> logger) : ICommandHandler<LongRunningCommand>
 {
@@ -10,11 +10,13 @@ sealed class LongRunningCommandHandler(ILogger<LongRunningCommandHandler> logger
 
         while (!ct.IsCancellationRequested) //periodically check if this job should be terminated
         {
-            logger.LogInformation("It's been {elapsed:0} seconds since the long running job was started...", DateTime.Now.Subtract(startTime).TotalSeconds);
-
-            await Task.Delay(1000);
+            logger.LogInformation("It's been {elapsed:0} seconds since the long running job was started...", ElapsedSeconds());
+            await Task.Delay(1000); //don't pass the token to methods that throw upon token cancellation.
         }
 
-        logger.LogWarning("the long running task was cancelled after {elapsed:0} seconds!", DateTime.Now.Subtract(startTime).TotalSeconds);
+        logger.LogWarning("the long running task was cancelled after {elapsed:0} seconds!", ElapsedSeconds());
+
+        double ElapsedSeconds()
+            => DateTime.Now.Subtract(startTime).TotalSeconds;
     }
 }
